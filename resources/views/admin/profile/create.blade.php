9e2f9757c59657a6975de68e5639558989323615
Ryuya <r.yasooka1@gmail.com>
2019-12-27 06:21:52 +0000

    PHP11課題提出

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,1 +1,13 @@
-aaa
+@extends('layouts.profile')
+
+@section('title', 'プロフィール作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成</h2>
+            </div>
+        </div>
+    </div
+@endsection
