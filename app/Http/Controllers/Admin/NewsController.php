6250480407a16bc3b0db3cc62953fe5d1a37b53e
EC2 Default User <ec2-user@ip-172-31-41-84.us-east-2.compute.internal>
2020-02-26 07:10:52 +0000

    S3対応

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\News;
 use App\History;
-
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -29,8 +29,8 @@ class NewsController extends Controller
 
     // formに画像があれば、保存する
     if (isset($form['image'])) {
-      $path = $request->file('image')->store('public/image');
-      $news->image_path = basename($path);
+      $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+      $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -78,8 +78,8 @@ class NewsController extends Controller
       // 送信されてきたフォームデータを格納する
       $news_form = $request->all();
       if (isset($news_form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
         unset($news_form['image']);
       } elseif (isset($request->remove)) {
         $news->image_path = null;
